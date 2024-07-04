90154fcbe21982276543fe1b5852a79d3156fd8a
Steven Erazo <erazoestiven1@gmail.com>
2024-07-04 17:23:40 -0500

    Pantalla registro usuario v1

---
+++ src/components/register/register.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "./register.css";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para manejar el envío del formulario
+    console.log(formData);
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Registro de clientes</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <h3>Nombres:</h3>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <h3> Apellidos: </h3>
+          <input
+            type="text"
+            name="lastname"
+            value={formData.lastname}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <h3>Numero de cedula:</h3>
+          <input
+            type="text"
+            name="ci"
+            value={formData.ci}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <h3>Email:</h3>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <h3>Contraseña:</h3>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Registrarse</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
